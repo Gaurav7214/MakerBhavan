87ea0edb17e05795fbf5d8fc944580206420830d
Gaurav Viramgami <58519896+Gaurav7214@users.noreply.github.com>
2020-09-17 16:57:51 +0530

    Font changes

--- index.js
+++
@@ -1,7 +0,0 @@
-$(window).on('resize', function() {
-    if($(window).height() > 500) {
-        $('#border').addClass('border-right');
-    }else{
-        $('#border').removeClass('border-right');
-    }
-})
